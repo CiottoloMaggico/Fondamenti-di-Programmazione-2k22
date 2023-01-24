d8e74d5c0ebf7026999b08282e626af4749da2bc
Ciottolomaggico <fantozz2003@outlook.com>
2023-01-24 13:21:02 +0100

    Updates

--- HW4-req/test.py
+++
@@ -1,6 +0,0 @@
-word = "ciaociao"
-
-for i in range(len(word)//2+1):
-    print(word[i], word[-(i+1)])
-lista = [0, 1]
-print(lista[(0 or 1)] == 0)
--- HW8-req/tests.py
+++
@@ -1,5 +0,0 @@
-
-with open("boards/01.txt", "r") as file:
-    board = [tuple(line.split()) for line in file.readlines()]
-    print(board)
-    print("".join(["".join(_) for _ in board]))
